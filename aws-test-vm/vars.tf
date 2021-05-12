b774b55f71fdc4ce5b8f926d5f36bb427f796ba1
Ferhat Güneri <ferhatguneri@outlook.com>
2021-05-12 14:29:48 +0300

    Create vars.tf

---
+++ aws-test-vm/vars.tf
@@ -0,0 +1,31 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-013f17f36f8b1fefb"
+    us-west-2 = "ami-02701bcdc5509e57b"
+    eu-west-1 = "ami-06fd78dc2f0b69910"
+
+}
+}
+
+variable "PATH_TO_PRIVATE_KEY" {
+  default = "mykey"
+}
+
+variable "PATH_TO_PUBLIC_KEY" {
+  default = "mykey.pub"
+}
+
+variable "INSTANCE_USERNAME" {
+  default = "ubuntu"
+}

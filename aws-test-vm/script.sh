df9c6041ba5c2a81718b2573f303e3bbd95d87fb
Ferhat Güneri <ferhatguneri@outlook.com>
2021-05-29 13:48:31 +0300

    add k3d kubectl helm installation in script.sh
    installing k3d kubectl and helm in virtual machine after creation

--- aws-test-vm/script.sh
+++ aws-test-vm/script.sh
@@ -5,11 +5,24 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
   sleep 1
 done
 
+#Install Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
 chmod +x get-docker.sh
 sh get-docker.sh
 
+#Install Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.28.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-
 sudo chmod +x /usr/local/bin/docker-compose
+
+#Install k3d
+wget -q -O - https://raw.githubusercontent.com/rancher/k3d/main/install.sh | bash
+
+#Install kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+mv kubectl /usr/local/bin/
+chmod +x /usr/local/bin/kubectl
+
+#Install helm
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
